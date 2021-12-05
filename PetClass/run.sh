20618c6fd9659058329f7d5a591ce5551d029456
peppapig450 <nbgenius1@gmail.com>
2021-12-04 16:32:20 -0800

    add else to the run.sh if statement and format run.sh

--- PetClass/run.sh
+++ PetClass/run.sh
@@ -1,38 +1,37 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
 checkCommands() {
-  if ! command -v javac &> /dev/null 
-  then 
-    echo "Java compiler not found, exiting.."
-    return 127 
-  fi 
+	if ! command -v javac &>/dev/null; then
+		echo "Java compiler not found, exiting.."
+		return 127
+	fi
 
-  if ! command -v java &> /dev/null 
-  then 
-    echo "Java not found, exiting.."
-    return 127 
-  fi 
-  if [[ $(uname) = 'Darwin' ]]; then 
-    find='gfind'
-  else 
-    find='find'
-  fi
+	if ! command -v java &>/dev/null; then
+		echo "Java not found, exiting.."
+		return 127
+	fi
+	if [[ $(uname) = 'Darwin' ]]; then
+		find='gfind'
+	else
+		find='find'
+	fi
 }
 
 run() {
-  if [ -z $($find . -maxdepth 1 -type f -name '*.class' -printf 1 -quit) ]; then 
-    java Main 
-  else 
-    javac "${files[@]}" && java Main
-  fi
+	if [ -z $($find . -maxdepth 1 -type f -name '*.class' -printf 1 -quit) ]; then
+		java Main
+	else
+		javac "${files[@]}" && java Main
+	fi
 }
 files=()
-checkCommands 
-if [ $? -eq 0 ]; then 
-  while IFS= read -r -d $'\0' f; do
-    f="${f:2}"
-    files+=("$f")
-  done < <(find . -type f -name '*.java' -print0)
-  run
-  fi
-exit 1
+checkCommands
+if [ $? -eq 0 ]; then
+	while IFS= read -r -d $'\0' f; do
+		f="${f:2}"
+		files+=("$f")
+	done < <(find . -type f -name '*.java' -print0)
+	run
+else
+	exit 1
+fi
